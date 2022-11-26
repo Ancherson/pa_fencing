9b945d0c9762cd36f0230919b4d14919667478a7
Ancherson <julescherion@gmail.com>
2022-11-26 14:48:52 +0100

    thread testing and satge

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import curses
 import os
-
+import numpy as np
 
 def start_stage():
     stdscr = curses.initscr()
@@ -16,15 +16,21 @@ def start_stage():
     i=0
     y=0
     while(True):
+        sys.stdin.reconfigure(newline=None)
         c = stdscr.getch()
         if c == ord('s'):
-            break
-        elif c == ord('v'):
-            break
+            stdscr.addstr(i,y,"s")
+            y+= 1
+            
+        elif c == ord('q'):
+            stdscr.addstr(i,y,"q")
+            y+= 1
         elif c == ord('c'):
-            break
-        elif c == ord('d'):
-            # stdscr.addstr(i,y,character_empty)
+            stdscr.addstr(i,y,"c")
+            y+= 1
+        elif c == curses.KEY_LEFT:
+            y-=1
+        elif c == ord('m'):
             break
     curses.nocbreak()
     stdscr.keypad(False)
@@ -34,7 +40,6 @@ def start_stage():
 def stage_read():
     if len(sys.argv) == 1:
         stage_file = os.path.abspath("stage1.ffscene")
-        
     else :
         if sys.argv[1].split(".")[-1] != "ffscene" :
             print("enter a .ffscene file please !")
@@ -44,8 +49,19 @@ def stage_read():
        return file.readline()
         
 
+def model(stage):
+    game_array = np.zeros(len(stage),dtype=int)
+    game_array[stage.find('1')] = 1
+    game_array[stage.find('2')] = 2
+    for (count,char) in enumerate(stage):
+        if char == 'x':
+            game_array[count] = 3
+    print(game_array)
+    
 
+# print("<o>\n |_/\n |\n/|   \u2588")
 
-print("<o>\n |_/\n |\n/|   \u2588")
+# print("############")
 
-print("############")
+stage = stage_read()
+model(stage)
--- thread.py
+++ thread.py
@@ -1,9 +1,10 @@
 from threading import Lock
 from threading import Thread
+import threading
 lock = Lock()
 
 i = 0
-
+y=0
 
 def task():
     for _ in range(100000):
@@ -13,7 +14,13 @@ def task():
         lock.release()
     print('done')
 
-
+def task2():
+    for _ in range(100000):
+        lock.acquire()
+        global y
+        y+= 1 
+        lock.release()
+    print('done')
 # create two new threads
 t1 = Thread(target=task)
 t2 = Thread(target=task)
@@ -24,4 +31,16 @@ t2.start()
 
 t1.join()
 t2.join()
-print(i)
+threads = []
+for j in range(10):
+    print(f"thread {j} launched")
+    thread = Thread(target=task2)
+    threads.append(thread)
+    thread.start()
+    
+print(threading.active_count())
+print(y)
+
+for th in threads:
+    th.join()
+print(y)
